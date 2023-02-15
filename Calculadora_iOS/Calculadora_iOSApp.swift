1514ec0047b6f4970967579386e96adb093a27ef
Pedrojimvi <pedrojv1030@gmail.com>
2023-02-15 11:53:00 +0100

    Initial Commit

---
+++ Calculadora_iOS/Calculadora_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculadora_iOSApp.swift
+//  Calculadora_iOS
+//
+//  Created by Usuario invitado on 15/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Calculadora_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculadora_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculadora_iOS
+//
+//  Created by Usuario invitado on 15/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
